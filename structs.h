6fc1f4b4ef24fecb54aa36d0888053cb105b5d48
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2018-08-07 16:50:47 +0900

    Add files via upload

---
+++ structs.h
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------------
+*	Name: Tristan Santiago
+*	Date: July 31, 2018
+*	Description: File: File: structs.h
+----------------------------------------------------------------------------*/
+/* You can modify the structure to store whatever you'd like in your BST */
+
+struct data {
+	int number;
+	char *name;
+};
